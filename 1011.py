6ad74ebccec9043ea05fad9375df0facd1909ecc
GeniusK <geniusk1047@naver.com>
2021-05-27 13:18:35 +0900

    1011.py 푸는중

---
+++ 1011.py
@@ -0,0 +1,22 @@
+# 푸는중
+t = int(input())
+cases = []
+for i in range(0, t):
+    cases.append(input())
+
+result = 0
+for case in cases:
+    x, y = case.split(' ')
+    x = int(x)
+    y = int(y)
+    finish = y - 1
+    move = 1
+    queue = [move]
+    while True:
+        if x == finish:
+            break
+        if x - 1 != finish:
+            
+
+result += 1
+print(result)
